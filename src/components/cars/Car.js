0f854cda6fcd0339366ead45ea167e15dabf3723
Edson Neri <veryneri8@gmail.com>
2019-06-21 11:25:53 -0500

    refactors `Car` with `AddCar` component

--- src/components/cars/Car.js
+++ src/components/cars/Car.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { CarById } from '../../graphql';
+import { AddCar } from '../../graphql';
 
 class Car extends React.Component {
     render() {
-        return <CarById id="a" />
+        return <AddCar />
     }
 }
 
