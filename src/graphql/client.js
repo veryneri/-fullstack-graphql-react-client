f213bce0daf2738fbb469127ca4d02c12a46de22
Edson Neri <veryneri8@gmail.com>
2019-06-21 19:51:30 -0500

    refactors `client` to use `WebSocketLink`

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,12 +1,36 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 const httpLink = new HttpLink({
     uri: 'http://localhost:4000/graphql',
 });
+const wsLink = new WebSocketLink({
+    options: {
+        reconnect: true,
+    },
+    uri: 'ws://localhost:4000/graphql',
+});
+const splitter = split(
+    ({ query }) => {
+        const {
+            kind,
+            operation,
+        } = getMainDefinition(query);
+
+        return (
+            kind === 'OperationDefinition' &&
+            operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
+);
 
 export default new ApolloClient({
     cache: new InMemoryCache(),
-    link: httpLink, 
+    link: splitter, 
 });
