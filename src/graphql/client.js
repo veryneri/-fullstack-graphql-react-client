f8739c8916b9906cfd8a6085b0a3020c9a767fc0
Edson Neri <veryneri8@gmail.com>
2019-06-21 19:05:03 -0500

    refactors `client` to use `ApolloClient`

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,5 +1,12 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+});
 
 export default new ApolloClient({
-    uri: 'http://localhost:4000'
+    cache: new InMemoryCache(),
+    link: httpLink, 
 });
